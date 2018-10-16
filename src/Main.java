412bb2daa026f6fe9af7a60636e8b080c8fdeba9
agnes-kabaly <alicet.woods@gmail.com>
2018-10-16 20:58:21 +0200

    scanner practice java files

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        ReadingUserInput readingUserInput = new ReadingUserInput();
+        //readingUserInput.sumTenNumber();
+
+        MinAndMaxInput minAndMaxInput = new MinAndMaxInput();
+        minAndMaxInput.minAndMaxInputChallenge();
+
+    }
+
+}
